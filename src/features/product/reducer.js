a79531e6929ef59b9c00a21fe74216e550da9973
devultimo <dev.ultimosolution@gmail.com>
2020-10-29 14:42:33 +0700

    immutable update state and change from filter to find

--- src/features/product/reducer.js
+++ src/features/product/reducer.js
@@ -16,28 +16,25 @@ export default function (state = initialState, action) {
         }
         case ADD_DATA:
         const currentState = state.cart;
-        var newCart = currentState.filter(function(item) {
-            return action.payload.id === item.id
-        })
+        const newCart = currentState.find((item) => action.payload.id === item.id )
 
-        if (newCart.length === 0 ) {
+        if (newCart === undefined ) {
             return {
                 ...state,
                 cart: [...state.cart, action.payload],
                 totalPrice: state.totalPrice + action.payload.price
             } 
         } else {
-            const test = newCart[0].counter + 1
-            newCart[0].counter = test ;
-            const temp = newCart[0];
-            let cartObj = state.cart.filter((item) => item.id !== action.payload.id );
-            console.log('cart obj',cartObj)
-            cartObj.push(temp);
+            const existingItem = Object.assign({}, newCart);
+            existingItem.counter = existingItem.counter + 1;
+            let cartObj = [...state.cart.filter((item) => item.id !== action.payload.id)];
+            cartObj.push(existingItem);
+    
             return {
-                ...state,
-                cart: cartObj,
-                totalPrice: state.totalPrice + action.payload.price
-            } 
+              ...state,
+              cart: cartObj,
+              totalPrice: state.totalPrice + action.payload.price,
+            };
         }
         case ADD_COUNTER:
             return {
